4e6265493ea848b8ed6ab3473e48b4b54d1ef449
unknown <rostand.siewe9@gmail.com>
2022-09-05 18:37:02 +0100

    cart screen error next method

---
+++ lib/views/order_confirmation/widget/horizontal_line.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+import '../../utils/size_config.dart';
+
+class HorizontalLine extends StatelessWidget {
+  final Color color;
+
+  const HorizontalLine({this.color = Colors.grey,Key? key}):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      height: getProportionateScreenHeight(1),
+      color: color,
+    );
+  }
+}
